3fd41ace18854b9fa7f69145995c37273ec23664
Calvin <calvin.freese@hotmail.com>
2019-03-26 17:06:04 -0500

    CF -- are we using onclick="editItem(${data.id}) ? ? otherwise, i must adjust (fx) modalEdit a BUNCH

--- app/static/to-do-script.js
+++ app/static/to-do-script.js
@@ -33,7 +33,7 @@
             <td>${data.overdue}</td>
             <td>${data.comment}</td>
             <td>${data.day}</td>
-            <td><button type="button"  data-toggle="modal" data-target="edit-modal" class="btn btn-outline-warning btn-sm editModal" onclick="editItem('${data.id}')">Edit Task</button></td>
+            <td><button type="button"  data-toggle="modal" data-target="edit-modal" class="btn btn-outline-warning btn-sm editModal" onclick="modalEdit('${data.id}')">Edit Task</button></td>
             <td><button class="btn btn-outline-danger btn-sm" onclick="removeItem('${data.id}')">Delete</button></td
             </tr>`;
         let list = document.querySelector(".todo-list")
